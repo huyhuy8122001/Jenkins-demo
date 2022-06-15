95f8d4f01cf3638467119920daa6ce67566eed9d
huyhuy8122001 <71826272+huyhuy8122001@users.noreply.github.com>
2022-06-15 18:19:38 +0700

    commit test jenkins

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home_page():
-    return " Hello Final Devnet Lab "
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
+print('hello jenkins')
