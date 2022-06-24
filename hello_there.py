283d2c4d8b051bf9ba470eea1ae01cd41622f973
huyQuang <huyhuy8122001@gmai.com>
2022-06-24 12:10:33 +0700

    build step 3

--- hello_there.py
+++ hello_there.py
@@ -1,1 +1,8 @@
-print('hello there !!')
+import requests
+
+response = requests.get('https://www.google.com')
+
+if response.status_code == 200:
+    print('Get successfully !!')
+else:
+    print('Get Failed !!')
