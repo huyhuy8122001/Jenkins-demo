1669eb69a1dd55b0c5ecc32a8272e202c1851319
huyQuang <huyhuy8122001@gmai.com>
2022-06-29 14:34:45 +0700

    update app

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return " Hello world "
+    return " Hello world !!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
- ####  Jenkins pipeline ####
-
- ##### test ####  
- ####
